8233f54c5313361ddba9998ab3fa0ab8eba3ed0e
juanlapradac <jplaprada@hotmail.com>
2023-10-03 20:42:06 -0700

    Se completo el examen

---
+++ examenculerote.js
@@ -0,0 +1,47 @@
+//PRIMER ARREGLO FILTER
+
+//let array = [2,5,6,8,10];
+
+//const numeroMayor = array.filter((array)=>array>=5);
+//numeroMayor.forEach((numeroMayor)=>{
+//console.log(numeroMayor);
+//});
+
+//SEGUNDO ARREGLO SLICE
+
+//let array = ['naranja', 'platano', 'manzana', 'fresa', 'kiwi'];
+
+//const slice = array.slice(2,4)
+//console.log(slice);
+
+//TERCER ARREGLO MAP
+//let array = [1,2,3,4];
+
+  
+  //array.map((array, index) => {
+    
+    //console.log(index, ":", array +10);
+  //});
+  
+
+
+  //CUARTO ARREGLO CONCAT
+
+  //let array1 = ['H','I','J'];
+  //let array2 = ['K','L','M'];
+  
+  //let resultado = array1.concat(array2); 
+  //console.log(resultado);
+
+//QUINTO ARREGLO INCLUDES
+ 
+let array = ["MEXICO", "USA", "FRANCIA"];
+
+let palabra = "FRANCIA";
+
+if (array.includes(palabra)) {
+  console.log(`Si EXISTE "${palabra}".`);
+} else {
+  console.log(`NO EXISTE "${palabra}".`);
+}
+
